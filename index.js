291cf85068989607b51e37646fbbab17c93ba4fd
Samantha Crespo <samkcrespo@gmail.com>
2022-04-07 14:20:45 -0700

    commit tree fafbc910f2d010cdc4e701e8264d07db5c182d63

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+
+const findMatching = (drivers, string) => {
+    let answer = drivers.filter(element => element === string || element === string.toLowerCase() )
+    return answer
+}
+
+function fuzzyMatch(drivers, string){
+    let firstLetter = drivers.filter(driver => driver.slice(0, string.length) === string)
+    return firstLetter
+}
+function matchName(drivers, string){
+    let name = drivers.filter(driver => driver.name === string)
+    return name
+}
 // Code your solution here
